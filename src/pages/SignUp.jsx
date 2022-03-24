9aaf2c52fb417accf6fbfa237e257f6081d02c2d
Felipe Machado <felipe.machado.front.end@gmail.com>
2022-03-24 18:18:44 -0300

    Mudando o dado do contexto

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,12 +3,21 @@ import { Context } from "../contexts/Context";
 
 const SignUp = () => {
   const { state, dispatch } = useContext(Context);
+  const handleChangeName = () => {
+    dispatch({
+      type: "CHANGE_NAME",
+      payload: {
+        name: "João",
+      },
+    });
+  };
   return (
     <div>
       <h2>Tela SignUp</h2>
       <p>
         {state.user.name} que tem a idade {state.user.idade} anos
       </p>
+      <button onClick={handleChangeName}>Trocar nome para João</button>
     </div>
   );
 };
